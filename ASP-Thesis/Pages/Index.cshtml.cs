dded15db2b7ebc01e6730084dd1b5368afdb463f
unLa4ky <unLa4ky@users.noreply.github.com>
2020-02-07 03:55:20 +0100

    Added project of ASP.NET and Bootstrap. Added some JSON like database.
    ASP-Thesis - Lazar folder
    Bootstrap - Nemanja folder

---
+++ ASP-Thesis/Pages/Index.cshtml.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using ASP_Thesis.Models;
+using ASP_Thesis.Services;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace ASP_Thesis.Pages
+{
+    public class IndexModel : PageModel
+    {
+        private readonly ILogger<IndexModel> _logger;
+        public JsonFileProductService ProductService;
+        public IEnumerable<Product> Products { get; private set; }
+
+        public IndexModel(
+            ILogger<IndexModel> logger,
+            JsonFileProductService productService
+            )
+        {
+            _logger = logger;
+            ProductService = productService;
+        }
+
+        public void OnGet()
+        {
+            Products = ProductService.GetProducts();
+        }
+    }
+}
