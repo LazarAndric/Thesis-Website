e3e522698447e9caac758929acd772fa508a506e
ne77a <49867018+neZza97@users.noreply.github.com>
2020-03-23 22:43:29 +0100

    update shop page

--- Undergraduate thesis/Undergraduate thesis/wwwroot/js/site.js
+++ Undergraduate thesis/Undergraduate thesis/wwwroot/js/site.js
@@ -2,3 +2,10 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your Javascript code.
+
+$(document).ready(function () {
+    $('.kategorija').click(function () {
+        var category = $(this).attr('id');
+
+    })
+});
