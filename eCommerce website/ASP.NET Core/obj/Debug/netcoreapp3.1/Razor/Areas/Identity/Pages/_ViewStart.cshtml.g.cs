bc80b2b48b6521dbf0c28d0ded128c0325b58f22
unLa4ky <unLa4ky@users.noreply.github.com>
2020-05-21 00:36:56 +0200

    Add  override for UI elements (login, register, etc.)

--- eCommerce website/ASP.NET Core/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
+++ eCommerce website/ASP.NET Core/obj/Debug/netcoreapp3.1/Razor/Areas/Identity/Pages/_ViewStart.cshtml.g.cs
@@ -12,7 +12,29 @@ namespace AspNetCore
     using Microsoft.AspNetCore.Mvc;
     using Microsoft.AspNetCore.Mvc.Rendering;
     using Microsoft.AspNetCore.Mvc.ViewFeatures;
+#nullable restore
+#line 1 "D:\PROJEKTI\GitHub\Thesis-Website\eCommerce website\ASP.NET Core\Areas\Identity\Pages\_ViewImports.cshtml"
+using Microsoft.AspNetCore.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 2 "D:\PROJEKTI\GitHub\Thesis-Website\eCommerce website\ASP.NET Core\Areas\Identity\Pages\_ViewImports.cshtml"
+using ASP.NET_Core.Areas.Identity;
+
+#line default
+#line hidden
+#nullable disable
+#nullable restore
+#line 3 "D:\PROJEKTI\GitHub\Thesis-Website\eCommerce website\ASP.NET Core\Areas\Identity\Pages\_ViewImports.cshtml"
+using ASP.NET_Core.Areas.Identity.Pages;
+
+#line default
+#line hidden
+#nullable disable
     [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"13b82ef97968a56a4de533f67cf3e21d662f41aa", @"/Areas/Identity/Pages/_ViewStart.cshtml")]
+    [global::Microsoft.AspNetCore.Razor.Hosting.RazorSourceChecksumAttribute(@"SHA1", @"14e6571a4082fc8ebb18b09e3c906821e2ca4ff9", @"/Areas/Identity/Pages/_ViewImports.cshtml")]
     public class Areas_Identity_Pages__ViewStart : global::Microsoft.AspNetCore.Mvc.Razor.RazorPage<dynamic>
     {
         #pragma warning disable 1998
