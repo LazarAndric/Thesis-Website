083f2b5dbd58034e60a8879538e87bc5f2c44752
ne77a <49867018+neZza97@users.noreply.github.com>
2020-06-08 19:44:01 +0200

    updating pages for register and login, and little changes on index page

--- eCommerce website/ASP.NET Core/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ eCommerce website/ASP.NET Core/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -47,17 +47,28 @@ namespace ASP.NET_Core.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
+            [Display(Name = "Ime")]
+            public string Name { get; set; }
+
+            [Required]
+            [EmailAddress]
+            [Display(Name = "Prezime")]
+            public string Lastname { get; set; }
+
+
+            [Required]
+            [EmailAddress]
             [Display(Name = "Email")]
             public string Email { get; set; }
 
             [Required]
             [StringLength(100, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Lozinka")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
+            [Display(Name = "Potvrda lozinke")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
         }
