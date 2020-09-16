f54310ae1eecb1a1309fb6a568070c5514f4d9d5
ne77a <49867018+neZza97@users.noreply.github.com>
2020-09-17 00:14:12 +0200

    change on contact page, add models, implement jwt in our site

---
+++ eCommerceWebsite/ASP.NET Core/Models/Product.cs
@@ -0,0 +1,36 @@
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System;
+using System.Collections.Generic;
+
+namespace Commander.Models
+{
+    [Table("tblProduct")]
+    public class Product
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [MaxLength]
+        public string Name { get; set; }
+        [MaxLength]
+        public string Description{get;set;}
+        [Required]
+        public int? CategoryId { get; set; }
+
+        [ForeignKey("CategoryId")]
+        public Category Category { get; set; }
+        [Required]
+        public float? Price { get; set; }
+        // [Required]
+        // public int Quantity{get; set;}
+        public bool IsOnSale { get; set; }
+        [Range(0,100)]
+        public int Sale {get;set;}
+        public string ImgUrl { get; set; }
+        public int NumberOfViews { get; set; }
+        public int NumberOfPurchases { get; set; }
+        public DateTime DateOfCreate { get; set; }
+        public bool IsShowInShop { get; set; }
+    }
+}
