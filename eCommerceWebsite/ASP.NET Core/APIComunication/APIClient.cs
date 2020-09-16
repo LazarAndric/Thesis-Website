ed62a55e210d60aba511304f4985a92fcc2b831b
test <lazarandric97@gmail.com>
2020-09-16 23:25:38 +0200

    Revert "Revert "Merge branch 'Backend-Merge' into MergeBranch""
    This reverts commit f47ecc536969aa1ada2af83adc0843394432f881.

--- eCommerceWebsite/ASP.NET Core/APIComunication/APIClient.cs
+++ eCommerceWebsite/ASP.NET Core/APIComunication/APIClient.cs
@@ -1,47 +1,62 @@
-﻿using System;
+﻿using Microsoft.Identity.Client;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Http;
+using System.Net.Http.Headers;
 using System.Threading.Tasks;
+using WebApplication1;
 
 namespace ASP.NET_Core.APIComunication
 {
     public class APIClient
     {
-        public void SetAPIClient()
+        public static APIClient Instance;
+        public AuthenticationResult result { get; set; }
+        public HttpClientHandler clientHandler { get; set; }
+        public AuthConfig config { get; set; }
+        public APIClient(AuthenticationResult result, HttpClientHandler clientHandler, AuthConfig config)
+        {
+            Instance = this;
+            this.result = result;
+            this.clientHandler = clientHandler;
+            this.config = config;
+        }
+        public async void SetAPIClient()
         {
 
-            //if(!string.IsNullOrEmpty(result.AccessToken))
-            //{
-            //    var httpClient = new HttpClient(clientHandler);
-            //    var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
+            if (!string.IsNullOrEmpty(result.AccessToken))
+            {
+                var httpClient = new HttpClient(clientHandler);
+                var defaultRequestHeaders = httpClient.DefaultRequestHeaders;
 
-            //    if(defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any
-            //        (m=> m.MediaType=="apolictaion/json"))
-            //    {
-            //        httpClient.DefaultRequestHeaders.Accept.Add(new
-            //            MediaTypeWithQualityHeaderValue("application/json"));
-            //    }
+                if (defaultRequestHeaders.Accept == null || !defaultRequestHeaders.Accept.Any
+                    (m => m.MediaType == "apolictaion/json"))
+                {
+                    httpClient.DefaultRequestHeaders.Accept.Add(new
+                        MediaTypeWithQualityHeaderValue("application/json"));
+                }
 
-            //    defaultRequestHeaders.Authorization =
-            //        new AuthenticationHeaderValue("bearer", result.AccessToken);
+                defaultRequestHeaders.Authorization =
+                    new AuthenticationHeaderValue("bearer", result.AccessToken);
 
-            //    HttpResponseMessage response = await httpClient.GetAsync(config.BaseAddress+"api/User");
+                HttpResponseMessage response = await httpClient.GetAsync(config.BaseAddress + "api/User");
 
-            //    if (response.IsSuccessStatusCode)
-            //    {
-            //        Console.ForegroundColor = ConsoleColor.Green;
-            //        string json = await response.Content.ReadAsStringAsync();
-            //        Console.WriteLine(json);
-            //    }
-            //    else
-            //    {
-            //        Console.ForegroundColor = ConsoleColor.Red;
-            //        Console.WriteLine($"Failed to call API: {response.StatusCode}");
-            //        string content = await response.Content.ReadAsStringAsync();
-            //        Console.WriteLine($"Content: {content}");
-            //    }
-            //    Console.ResetColor();
-            //}
+                if (response.IsSuccessStatusCode)
+                {
+                    Console.ForegroundColor = ConsoleColor.Green;
+                    string json = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine(json);
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine($"Failed to call API: {response.StatusCode}");
+                    string content = await response.Content.ReadAsStringAsync();
+                    Console.WriteLine($"Content: {content}");
+                }
+                Console.ResetColor();
+            }
         }
         public void Connect()
         {
